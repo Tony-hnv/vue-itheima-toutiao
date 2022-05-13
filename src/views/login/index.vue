a0dcd7eb017a04933436e489453c586487e2f118
Tony-hnv <2044131172@qq.com>
2022-05-13 13:35:26 +0800

    sec-two

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,44 +1,44 @@
 <template>
-    <div class="login-container">
-        <!-- 导航栏 -->
-        <van-nav-bar class="page-nav-bar" title="登录" />
+  <div class="login-container">
+    <!-- 导航栏 -->
+    <van-nav-bar class="page-nav-bar" title="登录" />
+    <!-- /导航栏 -->
 
-        <!-- 登陆的表单 -->
-        <van-cell-group>
-        <van-field
-            name="手机号码"
-            placeholder="请输入手机号"
-            >
-            <i slot="left-icon" class="toutiao toutiao-shouji"></i>
-            </van-field>
-
-        <van-field
-            name="验证码"
-            placeholder="请输入验证码"
-        >
+    <!-- 登录表单 -->
+    <van-form @submit="onSubmit">
+      <van-field
+        name="用户名"
+        placeholder="请输入手机号"
+      >
+        <i slot="left-icon" class="toutiao toutiao-shouji"></i>
+      </van-field>
+      <van-field
+        type="password"
+        name="验证码"
+        placeholder="请输入验证码"
+      >
         <i slot="left-icon" class="toutiao toutiao-yanzhengma"></i>
-        <van-button round class="send-sms-btn" slot="button" size="small" type="default">发送验证码</van-button>
-        </van-field>
-
-        <div class="login-btn-wrap" style="margin: 16px;">
-            <van-button class="login-btn" block type="info" native-type="submit">
-                登录
-                </van-button>
-        </div>
-
-        </van-cell-group>
-
-    </div>
+        <template #button>
+          <van-button class="send-sms-btn" round size="small" type="default">发送验证码</van-button>
+        </template>
+      </van-field>
+      <div class="login-btn-wrap">
+        <van-button class="login-btn" block type="info" native-type="submit">
+          登录
+        </van-button>
+      </div>
+    </van-form>
+    <!-- /登录表单 -->
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Login',
+  name: 'LoginIndex',
   components: {},
   props: {},
   data () {
     return {
-
     }
   },
   computed: {},
@@ -53,28 +53,25 @@ export default {
 }
 </script>
 
-<style lang="less" scoped>
-  .login-container {
-    .toutiao {
-    font-size:37px;
+<style scoped lang="less">
+.login-container {
+  .toutiao {
+    font-size: 37px;
   }
-
-  .send-sms-btn{
-    width:152px;
-    height:46px;
-    line-height:46px;
-    font-size:22px;
-    color:#666;
-    background-color:#ededed;
+  .send-sms-btn {
+    width: 152px;
+    height: 46px;
+    line-height: 46px;
+    background-color: #ededed;
+    font-size: 22px;
+    color: #666;
   }
-
   .login-btn-wrap {
-    padding:53px 33px;
-    .login-btn{
-      background-color:#6db4fb;
-      border:none;
+    padding: 53px 33px;
+    .login-btn {
+      background-color: #6db4fb;
+      border: none;
     }
   }
 }
-
 </style>
