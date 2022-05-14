72e4bafabc69779bc16504704742e3093c66e25f
Tony-hnv <2044131172@qq.com>
2022-05-14 18:08:32 +0800

    api可用了

--- src/api/user.js
+++ src/api/user.js
@@ -6,7 +6,7 @@ import request from '@/utils/request.js'
 export const login = data => {
   return request({
     methods: 'POST',
-    url: 'app/v1_0/authorizations',
+    url: '/v1_0/authorizations',
     data
   })
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -5,7 +5,7 @@
 import axios from 'axios'
 
 const request = axios.create({
-  baseURL: 'http://ttapi.research.itcast.cn/' // 基础路径
+  baseURL: 'http://toutiao.itheima.net' // 基础路径
 })
 
 // 请求拦截器
