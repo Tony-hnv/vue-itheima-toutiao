e8446cfab24eb7328a7126ddfeff5f4ad8cd315c
Tony-hnv <2044131172@qq.com>
2022-05-15 18:21:51 +0800

    finish not-login banner

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="my-cntainer">我的</div>
+  <div class="my-cntainer">
+    <div @click="$router.push('/login')" class="header not-login">
+      <div class="login-btn">
+        <img class="mobile-img" src="~@/assets/mobile.png" alt="">
+        <span class="text">登录/注册</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -21,5 +28,31 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+.my-cntainer {
+  .header {
+    height: 361px;
+    background: url('~@/assets/banner.png');
+    background-size: cover;
+    }
+  .not-login {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .login-btn {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      .mobile-img {
+        height: 132px;
+        width: 132px;
+        margin-bottom: 15px;
+      }
+      .text {
+        font-size: 28px;
+        color: #fff ;
+      }
+    }
+  }
+}
 </style>
