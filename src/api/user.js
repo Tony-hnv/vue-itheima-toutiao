5a9892f70f4d2f510a11cc40a05bd54c227582b3
Tony-hnv <2044131172@qq.com>
2022-05-16 16:07:03 +0800

    基本完成个人页面的设计交互

--- src/api/user.js
+++ src/api/user.js
@@ -2,7 +2,7 @@
 * 用户相关请求模块
 */
 import request from '@/utils/request.js'
-import store from '@/store'
+// import store from '@/store'
 
 export const login = data => {
   return request({
@@ -30,10 +30,12 @@ export const sendSms = mobile => {
 export const getUserInfo = () => {
   return request({
     method: 'GET',
-    url: '/v1_0/user',
+    url: '/v1_0/user'
     // 发送请求头数据
-    headers: {
-      Authorization: `Bearer ${store.state.user.token}`
-    }
+    // headers: {
+    //   // 注意：该接口需要授权才能访问
+    //   // token 的数据格式：Bearer token数据，注意 Bearer 后面有一个空格
+    //   Authorization: `Bearer ${store.state.user.token}`
+    // }
   })
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -3,13 +3,29 @@
  */
 
 import axios from 'axios'
+import store from '@/store'
 
 const request = axios.create({
   baseURL: 'http://toutiao.itheima.net' // 基础路径
 })
 
 // 请求拦截器
+// Add a request interceptor
+request.interceptors.request.use(function (config) {
+  // 请求发起会经过这里
+  // config：本次请求的请求配置对象
+  // console.log(config)
+  const { user } = store.state
+  if (user && user.token) {
+    config.headers.Authorization = `Bearer ${user.token}`
+  }
 
+  // 注意： 这里务必要返回 config 配置对象，否则请求就停在这里出不去了
+  return config
+}, function (error) {
+  // 如果请求出错（还没发出去）会进入这里
+  return Promise.reject(error)
+})
 // 相应拦截器
 
 export default request
