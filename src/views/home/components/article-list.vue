213d5079352c1ce268428cdd7e884c4b4269b283
mate <mate>
2022-05-16 22:39:12 +0800

    模拟失败

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -25,6 +25,8 @@
         v-model="loading"
         :finished="finished"
         finished-text="没有更多了"
+        :error.sync="error"
+        error-text="请求失败，点击重新加载"
         @load="onLoad"
         >
         <van-cell
@@ -51,7 +53,8 @@ export default {
       list: [], // 存储列表数据的数组
       loading: false, // 控制加载中 loading 状态
       finished: false, // 控制数据加载结束的状态
-      timestamp: null // 请求获取下一页数据的时间戳
+      timestamp: null, // 请求获取下一页数据的时间戳
+      error: false // 控制列表加载失败的提示状态
     }
   },
   computed: {},
@@ -91,6 +94,11 @@ export default {
           // 用于请求之后数据的时间戳会在当前请求结果中返回给你
           timestamp: this.timestamp || Date.now()
         })
+        
+        // 模拟错误
+        if (Math.random() > 0.5) {
+          JSON.parse('nijjnsdjn')
+        }
 
         // 2.把请求结果数据放到 list 数组中
         const { results } = data.data
@@ -108,8 +116,11 @@ export default {
         // 没有数据了，将finished 设置为 true, 不再load加载更多
           this.finished = true
         }
-      } catch (error) {
-        console.log('请求失败', error)
+      } catch (err) {
+        // 展示错误提示状态
+        this.error = true
+        // 请求失败了，loading也需要关闭
+        this.loading = false
       }
     }
   }
