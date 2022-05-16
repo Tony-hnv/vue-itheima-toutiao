41da8b1d90f85c7275ed4c63fffbf1d14c1556a3
Tony-hnv <2044131172@qq.com>
2022-05-16 17:54:27 +0800

    finish the home page's  layout

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -11,36 +11,137 @@
         icon="search"
         >搜索</van-button>
       </van-nav-bar>
-
     <!-- /导航栏 -->
+
+    <!-- 频道列表 -->
+    <!-- 通过 v-model 绑定当前激活标签对应的索引值，默认情况下启用第一个标签。
+        通过 animated 属性可以开启切换标签内容时的转场动画。
+        通过 swipeable 属性可以开启滑动切换标签页。
+    -->
+    <van-tabs
+    class="channel-tab" v-model="active" animated swipeable>
+      <van-tab
+      :title="channel.name"
+      v-for="channel in channels"
+      :key="channel.id"
+      >{{channel.name}}
+      </van-tab>
+      <div slot="nav-right" class="placeholder"></div>
+      <div slot="nav-right" class="hangburger-btn">
+        <i class="toutiao toutiao-gengduo"></i>
+      </div>
+    </van-tabs>
+    <!-- /频道列表 -->
   </div>
 </template>
 
 <script>
+import { getUserChannels } from '@/api/user'
+
 export default {
   name: 'HomeIndex',
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      active: 0,
+      channels: []
+    }
   },
   computed: {},
   watch: {},
-  created () {},
+  created () {
+    this.loadChannels()
+  },
   mounted () {},
-  methods: {}
+  methods: {
+    async loadChannels () {
+      try {
+        const { data } = await getUserChannels()
+        this.channels = data.data.channels
+        console.log(data)
+      } catch (error) {
+        this.$toast('获取频道数据失败')
+      }
+    }
+  }
 
 }
 </script>
 
 <style lang="less" scoped>
 .home-container {
-    .van-nav-bar__title {
-      max-width: unset
+    /deep/ .van-nav-bar__title {
+      max-width: unset;
     }
     .search-btn {
       width: 555px;
       height: 44px;
+      background-color: #5babfb;
+      border: none;
+      font-size: 28px;
+      .van-icon {
+        font-size: 32px;
+      }
+    }
+
+    /deep/ .channel-tab {
+      .van-tabs__wrap {
+        height: 82px;
+      }
+
+      .van-tab {
+        border-right: 1px solid #edeff3;
+        min-width: 200px;
+        font-size: 30px;
+        color: #777777;
+      }
+
+      .van-tab--active {
+        color: #333333;
+      }
+
+      .van-tabs__nav {
+        padding-bottom: 0;
+      }
+      .van-tabs__line {
+        width: 31px !important;
+        height: 6px;
+        background-color: #3296fa;
+        bottom: 8px;
+      }
+
+      .placeholder {
+        // 设置flex-shrink:0可以使它不参与父组件设置的flex界面布局计算
+        flex-shrink: 0;
+        width: 66px;
+        height: 82px;
+      }
+
+      .hangburger-btn {
+        position: fixed;
+        right: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 66px;
+        height: 82px;
+        background-color: #fff;
+        opacity: 0.902;
+        i.toutiao {
+          font-size: 33px;
+        }
+        &:before {
+          content: '';
+          position: absolute;
+          left: 0;
+          width: 1px;
+          height: 100%;
+          background: url('~@/assets/gradient-gray-line.png');
+          background-size: contain;
+        }
+
+      }
     }
 }
 </style>
