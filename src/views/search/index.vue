1005fdb051d184753079d47b28797e853fa87369
Tony-hnv <2044131172@qq.com>
2022-05-19 19:02:55 +0800

    历史记录

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -1,73 +1,97 @@
 <template>
   <div class="search-container">
-    <!-- 顶部搜索栏 -->
+    <!-- 搜索栏 -->
     <!--
-      Tips: 在 van-search 外层增加 form 标签，
-      且 action 不为空，即可在 iOS 输入法中显示搜索按钮。
-      -->
-    <form action="/" class="search-form">
+      Tips: 在 van-search 外层增加 form 标签，且 action 不为空，即可在 iOS 输入法中显示搜索按钮
+     -->
+    <form class="search-form" action="/">
       <van-search
         v-model="searchText"
         show-action
-        background="#3296fa"
         placeholder="请输入搜索关键词"
+        background="#3296fa"
         @search="onSearch"
         @cancel="onCancel"
         @focus="isResultShow = false"
-  />
-</form>
-<!-- /顶部搜索栏 -->
+      />
+    </form>
+    <!-- /搜索栏 -->
 
-<!-- 搜索结果 -->
-<search-result
-  v-if="isResultShow"
-  :search-text="searchText"
-  />
-<!-- /搜索结果 -->
+    <!-- 搜索结果 -->
+    <search-result
+      v-if="isResultShow"
+      :search-text="searchText"
+    />
+    <!-- /搜索结果 -->
 
-<!-- 联想建议 -->
-<search-suggestion
-  :search-text="searchText"
-  v-else-if="searchText"
-  @search="onSearch" />
-<!-- /联想建议 -->
+    <!-- 联想建议 -->
+    <search-suggestion
+      v-else-if="searchText"
+      :search-text="searchText"
+      @search="onSearch"
+    />
+    <!-- /联想建议 -->
 
-<!-- 搜索历史纪录 -->
-<search-history v-else />
-<!-- /搜索历史纪录 -->
-
-</div>
+    <!-- 搜索历史记录 -->
+    <search-history
+      v-else
+      :search-histories="searchHistories"
+      @clear-search-histories="searchHistories = []"
+      @search="onSearch"
+    />
+    <!-- /搜索历史记录 -->
+  </div>
 </template>
 
 <script>
-import searchHistory from './components/search-history'
-import searchSuggestion from './components/search-suggestion'
-import searchResult from './components/search-result'
+import SearchHistory from './components/search-history'
+import SearchSuggestion from './components/search-suggestion'
+import SearchResult from './components/search-result'
+import { setItem, getItem } from '@/utils/storage'
 
 export default {
   name: 'SearchIndex',
   components: {
-    searchHistory,
-    searchSuggestion,
-    searchResult
+    SearchHistory,
+    SearchSuggestion,
+    SearchResult
   },
   props: {},
   data () {
     return {
       searchText: '',
-      isResultShow: false // 控制搜索结果的展示
+      isResultShow: false, // 控制搜索结果的展示
+      searchHistories: getItem('TOUTIAO_SEARCH_HISTORIES') || [] // 搜索的历史记录数据
     }
   },
   computed: {},
-  watch: {},
+  watch: {
+    searchHistories (value) {
+      setItem('TOUTIAO_SEARCH_HISTORIES', value)
+    }
+    // searchHistories: {
+    //   handler () {}
+    // }
+  },
   created () {},
   mounted () {},
   methods: {
     onSearch (val) {
-      console.log(val)
+      // 更新文本框内容
       this.searchText = val
+
+      // 存储搜索历史记录
+      // 要求：不要有重复历史记录、最新的排在最前面
+      const index = this.searchHistories.indexOf(val)
+      if (index !== -1) {
+        this.searchHistories.splice(index, 1)
+      }
+      this.searchHistories.unshift(val)
+
+      // 渲染搜索结果
       this.isResultShow = true
     },
+
     onCancel () {
       this.$router.back()
     }
@@ -75,14 +99,12 @@ export default {
 }
 </script>
 
-<style lang="less" scoped>
+<style scoped lang="less">
 .search-container {
   padding-top: 108px;
-
   .van-search__action {
     color: #fff;
   }
-
   .search-form {
     position: fixed;
     top: 0;
