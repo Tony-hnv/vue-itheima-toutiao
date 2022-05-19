af07c8521d038824c2264bb7d592ad5551d10abd
Tony-hnv <2044131172@qq.com>
2022-05-19 14:40:34 +0800

    master

---
+++ src/views/search/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="search-container">
+    搜索页面
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchPage',
+  components: {},
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
